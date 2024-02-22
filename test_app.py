65d71deaf7f38f1ba4e0fb0ac918d4ecc7ba6de5
Yammy468 <yzhu0806@g.educaand.es>
2024-02-22 10:59:11 +0100

    subir .py de prueba3

--- test_app.py
+++ test_app.py
@@ -1,30 +1,25 @@
-""" Test de la función suma para ejemplo devops
+"""Pruebas para la función suma del módulo app."""
 
-    El tercero de los test debe fallar para probar Action
-"""
 import unittest
 from app import suma
 
-
 class SumaTest(unittest.TestCase):
-    """ Docstring class """
+    """Clase de pruebas para la función suma."""
 
-    def test1(self):
-        """ Docstring def
-        """
-        res = suma(1, 5)
-        self.assertEqual(res, 6)
+    def test_suma_numeros_positivos(self):
+        """Prueba la suma de dos números positivos."""
+        resultado = suma(1, 2)
+        self.assertEqual(resultado, 3)
 
-    def test2(self):
-        """ Docstring def """
-        res = suma(0, 0)
-        self.assertEqual(res, 0)
+    def test_suma_cero(self):
+        """Prueba la suma de cero con un número positivo."""
+        resultado = suma(0, 7)
+        self.assertEqual(resultado, 7)
 
-    def test3(self):
-        """ Esta prueba DEBE FALLAR"""
-        res = suma("1", "4")
-        self.assertEqual(res, 5)
-
+    def test_suma_numeros_invalidos(self):
+        """Prueba la suma de dos valores no enteros."""
+        resultado = suma("1", "2")
+        self.assertIsNone(resultado, "La función no debe aceptar valores no enteros")
 
 if __name__ == "__main__":
     unittest.main()
