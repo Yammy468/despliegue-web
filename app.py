625ec282c71df7a4cd1ccf5e56054b0666ede802
Yammy468 <yzhu0806@g.educaand.es>
2024-02-22 10:19:52 +0100

    revisado error2 paylint

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-"""Módulo para demostración de suma.
-
-Este módulo provee una función de suma que toma dos enteros y retorna su suma.
-"""
+""" Docstring del módulo """
 
 def suma(a: int, b: int) -> int:
-    """Suma dos números enteros.
-
-    Args:
-        a (int): Primer número entero.
-        b (int): Segundo número entero.
-
-    Returns:
-        int: La suma de `a` y `b`.
-    """
-    # La variable `x` ha sido removida ya que no se usa,
-    # lo cual mejora la claridad y el rendimiento del código.
     return a + b
 
