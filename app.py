53935cc18675af94ee906540b0d2621a34d69b3d
Yammy468 <yzhu0806@g.educaand.es>
2024-02-22 10:10:22 +0100

    revisado error paylint

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-""" Docstring del módulo """
+"""Módulo para demostración de suma.
 
+Este módulo provee una función de suma que toma dos enteros y retorna su suma.
+"""
 
 def suma(a: int, b: int) -> int:
-    """ docstring function
-    # debe dar errores en linter
+    """Suma dos números enteros.
+
+    Args:
+        a (int): Primer número entero.
+        b (int): Segundo número entero.
+
+    Returns:
+        int: La suma de `a` y `b`.
     """
-    x = "no es usable"
+    # La variable `x` ha sido removida ya que no se usa,
+    # lo cual mejora la claridad y el rendimiento del código.
     return a + b
+
